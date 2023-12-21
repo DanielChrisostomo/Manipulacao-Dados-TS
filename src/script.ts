e083ad344acbe518671aa70599796ec250e07470
DanielChrisostomo <daniluz1515@gmail.com>
2023-12-21 17:50:55 -0300

    Fetch Data

---
+++ src/script.ts
@@ -0,0 +1,33 @@
+import fetchData from './fetchData.js';
+
+type TransacaoPagamento = 'Boleto' | 'Cartão de Crédito';
+type TransacaoStatus =
+  | 'Paga'
+  | 'Recusada pela operadora de cartão'
+  | 'Aguardando pagamento'
+  | 'Estornada';
+
+interface TransacaoAPI {
+  Nome: string;
+  ID: number;
+  Data: string;
+  Status: TransacaoStatus;
+  Email: string;
+  ['Valor (R$)']: string;
+  ['Forma de Pagamento']: TransacaoPagamento;
+  ['Cliente Novo']: number;
+}
+
+async function handleData() {
+  const data = await fetchData<TransacaoAPI[]>(
+    'https://api.origamid.dev/json/transacoes.json',
+  );
+  console.log(data);
+
+  if (data) {
+    data.forEach((item) => {
+      console.log(item);
+    });
+  }
+}
+handleData();
