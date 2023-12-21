f217b26a83a879fc6d9d2d93250f846dbbf73f5f
DanielChrisostomo <daniluz1515@gmail.com>
2023-12-21 18:50:20 -0300

    Normalizar Transação

--- src/script.ts
+++ src/script.ts
@@ -1,33 +1,12 @@
 import fetchData from './fetchData.js';
-
-type TransacaoPagamento = 'Boleto' | 'Cartão de Crédito';
-type TransacaoStatus =
-  | 'Paga'
-  | 'Recusada pela operadora de cartão'
-  | 'Aguardando pagamento'
-  | 'Estornada';
-
-interface TransacaoAPI {
-  Nome: string;
-  ID: number;
-  Data: string;
-  Status: TransacaoStatus;
-  Email: string;
-  ['Valor (R$)']: string;
-  ['Forma de Pagamento']: TransacaoPagamento;
-  ['Cliente Novo']: number;
-}
+import normalizarTransacao from './normalizarTransacao.js';
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
     'https://api.origamid.dev/json/transacoes.json',
   );
-  console.log(data);
-
-  if (data) {
-    data.forEach((item) => {
-      console.log(item);
-    });
-  }
+  if (!data) return;
+  const transacoes = data.map(normalizarTransacao);
+  console.log(transacoes);
 }
 handleData();
